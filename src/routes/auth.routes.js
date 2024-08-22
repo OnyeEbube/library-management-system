0a01fe7a3e18745ecbdc3209817fc3afbcbf8057
OnyeEbube <okontaonyeka@gmail.com>
2024-08-22 16:50:58 +0100

    adding await to isPasswordMatch

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const { AuthController } = require("../controllers/auth.controller");
-const { userAuth, verifyUser } = require("../middleware/jwt.middleware");
+const {
+	userAuth,
+	verifyUser,
+	adminAuth,
+} = require("../middleware/jwt.middleware");
 const router = express.Router();
 
 router.post("/register", AuthController.createUser);
-router.post("/login" /*, userAuth*/, AuthController.loginUser);
+router.post("/login", AuthController.loginUser);
 router.post("/forgot-password", AuthController.forgotPassword);
 router.put("/reset-password", AuthController.resetPassword);
 router.get("/verify-token/:resetToken", AuthController.verifyToken);
 router.get("/", AuthController.getUsers);
 router.get("/me", userAuth, AuthController.getUser);
-router.delete("/:id", AuthController.deleteUser);
-router.patch("/:id", AuthController.updateUser);
+router.delete("/:id", adminAuth, AuthController.deleteUser);
+router.patch("/:id", adminAuth, userAuth, AuthController.updateUser);
 //router.get("/logout", AuthController.logoutUser);
 //router.get("/uploads/:fileName", AuthController.getProfilePicture);
 router.post("/uploads/:id", verifyUser, AuthController.uploadImage);
