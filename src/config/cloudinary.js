297eba25ebc9f702dfb7328972f9c4e8739c1e67
OnyeEbube <okontaonyeka@gmail.com>
2024-09-02 12:23:33 +0100

    ll

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,11 +1,12 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const cloudinary = require("cloudinary").v2;
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const api_key = process.env.CLOUDINARY_API_KEY;
+const api_secret = process.env.CLOUDINARY_API_SECRET;
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: cloud_name,
+	api_key: api_key,
+	api_secret: api_secret,
 });
 
 module.exports = cloudinary;
